490bb4627037d0b511a1bba20efa418f08c81251
pavanpodila <pavan@vyuh.tech>
2024-10-09 15:01:35 +0530

    fixed the external link icon

--- astro.config.ts
+++ astro.config.ts
@@ -10,6 +10,8 @@ import tailwind from '@astrojs/tailwind'
 import { sidebar } from './sidebar.ts'
 import rehypeExternalLinks from 'rehype-external-links'
 
+import icon from 'astro-icon';
+
 console.log(import.meta.env.DEV)
 
 // https://astro.build/config
@@ -26,71 +28,67 @@ export default defineConfig({
       ],
     ],
   },
-  integrations: [
-    starlight({
-      title: 'Docs',
-      description:
-        'A framework to build Modular, CMS-driven Flutter Apps. At Scale.',
-      tagline:
-        'A framework to build Modular, CMS-driven Flutter Apps. At Scale.',
-      logo: {
-        light: './src/assets/logo-light.png',
-        dark: './src/assets/logo-dark.png',
-      },
-      social: {
-        github: 'https://github.com/vyuh-tech/vyuh',
-        'x.com': 'https://x.com/vyuh_tech',
-        discord: 'https://discord.gg/b49sbjqszG',
-        youtube: 'https://youtube.com/@vyuh_tech',
-      },
-      // Add Open Graph images.
-      head: [
-        {
-          tag: 'meta',
-          attrs: {
-            property: 'og:image',
-            // The absolute URL of the image to use.
-            content: 'https://docs.vyuh.tech/social-card.png',
-          },
+  integrations: [starlight({
+    title: 'Docs',
+    description:
+      'A framework to build Modular, CMS-driven Flutter Apps. At Scale.',
+    tagline:
+      'A framework to build Modular, CMS-driven Flutter Apps. At Scale.',
+    logo: {
+      light: './src/assets/logo-light.png',
+      dark: './src/assets/logo-dark.png',
+    },
+    social: {
+      github: 'https://github.com/vyuh-tech/vyuh',
+      'x.com': 'https://x.com/vyuh_tech',
+      discord: 'https://discord.gg/b49sbjqszG',
+      youtube: 'https://youtube.com/@vyuh_tech',
+    },
+    // Add Open Graph images.
+    head: [
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'og:image',
+          // The absolute URL of the image to use.
+          content: 'https://docs.vyuh.tech/social-card.png',
         },
-        {
-          tag: 'meta',
-          attrs: {
-            property: 'og:site_name',
-            // The absolute URL of the image to use.
-            content: 'Docs for the Vyuh Framework',
-          },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'og:site_name',
+          // The absolute URL of the image to use.
+          content: 'Docs for the Vyuh Framework',
         },
-        {
-          tag: 'meta',
-          attrs: {
-            property: 'twitter:image',
-            // The absolute URL of the image to use.
-            content: 'https://docs.vyuh.tech/social-card.png',
-          },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'twitter:image',
+          // The absolute URL of the image to use.
+          content: 'https://docs.vyuh.tech/social-card.png',
         },
-      ],
-      components: {
-        Head: './src/components/starlight/Head.astro',
-        SocialIcons: './src/components/starlight/SocialIcons.astro',
-        Footer: './src/components/starlight/Footer.astro',
-        SkipLink: './src/components/starlight/SkipLink.astro',
       },
-      customCss: ['./src/tailwind.css'],
-      sidebar,
-      expressiveCode: {
-        // @ts-ignore
-        plugins: [pluginLineNumbers()],
-      },
-      plugins: [starlightLinksValidator()],
-      editLink: {
-        baseUrl: 'https://github.com/vyuh-tech/docs/edit/main/',
-      },
-      lastUpdated: true,
-    }),
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+    ],
+    components: {
+      Head: './src/components/starlight/Head.astro',
+      SocialIcons: './src/components/starlight/SocialIcons.astro',
+      Footer: './src/components/starlight/Footer.astro',
+      SkipLink: './src/components/starlight/SkipLink.astro',
+    },
+    customCss: ['./src/tailwind.css'],
+    sidebar,
+    expressiveCode: {
+      // @ts-ignore
+      plugins: [pluginLineNumbers()],
+    },
+    plugins: [starlightLinksValidator()],
+    editLink: {
+      baseUrl: 'https://github.com/vyuh-tech/docs/edit/main/',
+    },
+    lastUpdated: true,
+  }), react(), tailwind({
+    applyBaseStyles: false,
+  }), icon()],
 })
